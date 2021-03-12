2b0414c09bd1eb62e713ea2d8cc479ecaf8047d3
ZhenxiKIm <8055kjh@gmail.com>
2021-03-12 23:08:26 +0900

    토비의스프링 챕터1 토이프로젝트 실습 추가

---
+++ src/main/java/com/bom/start/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,23 @@
+package com.bom.start.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+    }
+
+
+}
+
