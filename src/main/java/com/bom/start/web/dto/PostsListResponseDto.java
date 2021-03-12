461226c82dc57a72800ba2b394cffc021c95173a
ZhenxiKIm <8055kjh@gmail.com>
2021-03-12 22:29:24 +0900

    CRUD 예제 작성

---
+++ src/main/java/com/bom/start/web/dto/PostsListResponseDto.java
@@ -0,0 +1,19 @@
+package com.bom.start.web.dto;
+
+import com.bom.start.domain.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+    }
+}
